037379849a377af59b343dea75f2c74c9b3edc2d
moss <moss.shen@okchem.com>
2017-03-14 23:51:36 +0800

    基本配置

---
+++ src/main/java/springbootDemo/Application.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Licensed to the OKChem
+ *
+ * http://www.okchem.com
+ *
+ *******************************************************************************/
+package springbootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author moss
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping(value = "/demo", method = RequestMethod.GET)
+	@ResponseBody
+	public String demo(){
+		return "hello world!";
+	}
+}
