199156711cea171d9dcac13c1360b94cf544eb1c
huanglei <Lei.huang@jieshunpay.cn>
2017-07-14 16:35:23 +0800

    其实不需要单独引入log  依赖包

--- src/main/java/springbootDemo/HomeController.java
+++ src/main/java/springbootDemo/HomeController.java
@@ -1,5 +1,6 @@
 package springbootDemo;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +12,12 @@ import entity.Entity;
 
 @RestController
 public class HomeController {
+	 private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	/**
 	 * http://localhost:8080/中文    不错，是可以支持中文的
 	 * http://localhost:8080/中文?name=黑妹&age=19&desc=美女一位
 	 * http://localhost:8080/中文?name=黑妹&age=19&desc=美女一位&hobbies=sdfs,werwr,rtwret,ertwert,ertwert
+	 * http://localhost:8080/中文?name=黑妹&age=19&desc=美女一位&hobbies=sdfs,werwr,rtwret,ertwert,ertwert&bodyDesc=身材不错
 	 * @param value
 	 * @return
 	 */
@@ -22,7 +25,7 @@ public class HomeController {
 	@ResponseBody
 	public String demo(@PathVariable String value,Entity entity,@RequestParam("bodyDesc") String bodyDesc){
 		
-		
+		logger.info("---------------~~~~~~~~~~~~23424242~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		
 		
 		return "welcome to us , "+entity.toString();
