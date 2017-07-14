802a2880a8087fafae56faece4520f09781ba7ab
huanglei <Lei.huang@jieshunpay.cn>
2017-07-14 10:57:48 +0800

    加个路径变量

--- src/main/java/springbootDemo/HomeController.java
+++ src/main/java/springbootDemo/HomeController.java
@@ -1,5 +1,6 @@
 package springbootDemo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	/**
+	 * http://localhost:8080/中文    不错，是可以支持中文的
+	 * @param value
+	 * @return
+	 */
+	@RequestMapping(value = "/{value}", method = RequestMethod.GET)
 	@ResponseBody
-	public String demo(){
-		return "welcome to us";
+	public String demo(@PathVariable String value){
+		
+		return "welcome to us  "+value;
 	}
 }
