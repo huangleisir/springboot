bc56f05e61ce1ee5aeacf460a14f134c959ee94f
huanglei <Lei.huang@jieshunpay.cn>
2017-07-14 18:05:34 +0800

    111

---
+++ src/main/java/config/InterceptorConfig.java
@@ -0,0 +1,27 @@
+package config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.hl.interceptor.MyInterceptor;
+
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+
+	/*@Autowired
+	MyInterceptor myInterceptor  ;*/
+	
+	
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        /*InterceptorRegistration csrfInterceptorRegistration = registry.addInterceptor(myInterceptor);
+        csrfInterceptorRegistration.addPathPatterns("/**");*/
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
+        super.addInterceptors(registry);
+
+    }
+}
