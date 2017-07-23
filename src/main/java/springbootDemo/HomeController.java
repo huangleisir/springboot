bdc6f3a7cede3080a7351f49754430f728217376
huangleisir <huangleisir@qq.com>
2017-07-23 10:53:25 +0800

    添加多环境配置文件  并使之生效

--- src/main/java/springbootDemo/HomeController.java
+++ src/main/java/springbootDemo/HomeController.java
@@ -15,10 +15,10 @@ import entity.Entity;
 public class HomeController {
 	 private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	/**
-	 * http://localhost:8080/中文    不错，是可以支持中文的
-	 * http://localhost:8080/中文?name=黑妹&age=19&desc=美女一位
-	 * http://localhost:8080/中文?name=黑妹&age=19&desc=美女一位&hobbies=sdfs,werwr,rtwret,ertwert,ertwert
-	 * http://localhost:8080/中文?name=黑妹&age=19&desc=美女一位&hobbies=sdfs,werwr,rtwret,ertwert,ertwert&bodyDesc=身材不错
+	 * http://localhost:9080/中文    不错，是可以支持中文的
+	 * http://localhost:9080/中文?name=黑妹&age=19&desc=美女一位
+	 * http://localhost:9080/中文?name=黑妹&age=19&desc=美女一位&hobbies=sdfs,werwr,rtwret,ertwert,ertwert
+	 * http://localhost:9080/中文?name=黑妹&age=19&desc=美女一位&hobbies=sdfs,werwr,rtwret,ertwert,ertwert&bodyDesc=身材不错
 	 * @param value
 	 * @return
 	 */
