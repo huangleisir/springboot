ab26edb579fb8bbdf66b7cd2593e389ee16819f0
huanglei <lei.huang@jieshunpay.cn>
2017-07-24 18:44:19 +0800

    add test url

--- src/main/java/springbootDemo/Application.java
+++ src/main/java/springbootDemo/Application.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 /**
  * @author  huanglei
+ * http://localhost:8080/demo
  */
 @SpringBootApplication
 @RestController
