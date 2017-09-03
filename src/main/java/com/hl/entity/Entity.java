69f377973622119e2cabff2e1279d1e02cc69a5f
huangleisir <huangleisir@qq.com>
2017-09-03 16:37:00 +0800

    learn sort members with fast key in entity

--- src/main/java/com/hl/entity/Entity.java
+++ src/main/java/com/hl/entity/Entity.java
@@ -3,38 +3,13 @@ package com.hl.entity;
 import java.util.List;
 
 public class Entity {
-	private String name;
 	private int age;
 	private String desc;
 	private List<String> hobbies;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public String getDesc() {
-		return desc;
-	}
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
+	private String name;
 	public Entity() {
 		super();
 		// TODO Auto-generated constructor stub
-	}
-	
-	public List<String> getHobbies() {
-		return hobbies;
-	}
-	public void setHobbies(List<String> hobbies) {
-		this.hobbies = hobbies;
 	}
 	public Entity(String name, int age, String desc, List<String> hobbies) {
 		super();
@@ -42,6 +17,31 @@ public class Entity {
 		this.age = age;
 		this.desc = desc;
 		this.hobbies = hobbies;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public List<String> getHobbies() {
+		return hobbies;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public void setHobbies(List<String> hobbies) {
+		this.hobbies = hobbies;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	@Override
 	public String toString() {
--- src/main/java/com/hl/service/impl/ServiceImpl.java
+++ src/main/java/com/hl/service/impl/ServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hl.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.hl.service.Service;
 
 public class ServiceImpl implements Service {
@@ -7,6 +10,11 @@ public class ServiceImpl implements Service {
 	@Override
 	public int plus(int a,int b) {
 		// TODO Auto-generated method stub
+		int f = 9;
+				int g = 10;
+				int c =11;
+				List list = new ArrayList();
+				
 		return a+b;
 	}
 
