4940a7fa54ab1e41321b4b81bb5ca8f4919c0f73
huanglei <lei.huang@jieshunpay.cn>
2017-09-27 18:11:36 +0800

    去掉多余行

--- src/main/java/com/hl/App.java
+++ src/main/java/com/hl/App.java
@@ -13,14 +13,12 @@ import java.util.Date;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
