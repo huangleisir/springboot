2b9d7b5bfdc9adc92475cc03ac478b1193a9c05e
huanglei <lei.huang@jieshunpay.cn>
2017-09-29 16:04:16 +0800

    add comment

--- src/main/java/com/jst/demo/controller/TestController.java
+++ src/main/java/com/jst/demo/controller/TestController.java
@@ -14,7 +14,12 @@ import io.swagger.annotations.ApiParam;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-
+		/**
+		 * 注意这个方法的请求，：     http://localhost:8866/demo/test/testdemo 
+		 * postman 用raw，请求体用json，{"appVersion":"34234"} contentType ： Json(application/json)
+		 * @param baseBo
+		 * @return
+		 */
 	 	@RequestMapping(value = "/testdemo", method = RequestMethod.POST)
 	    @ResponseBody
 	    public Result test(@ApiParam(name="BaseBo",value="测试baseBo",required=true)@RequestBody BaseBo baseBo) {
