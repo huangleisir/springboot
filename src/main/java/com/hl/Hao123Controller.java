36048e2556a90b47035e38e9866f7fc96d79e9ef
huangleisir <huangleisir@qq.com>
2018-03-21 21:04:56 +0800

    111

--- src/main/java/com/hl/Hao123Controller.java
+++ src/main/java/com/hl/Hao123Controller.java
@@ -1,13 +1,13 @@
 package com.hl;
 import java.io.IOException;
-import javax.servlet.ServletException;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hao123Controller {
@@ -27,9 +27,9 @@ public class Hao123Controller {
 	
 	@RequestMapping(value = "/baidu", method = RequestMethod.GET)
 	public void baidu(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~wangyi~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info("---------------~~~~~~~~~~~~baidu~~~~~"+getIpAddress(req));
 		try {
-			resp.sendRedirect("http://www.baidu.com"); 
+			resp.sendRedirect(new Date()+"http://www.baidu.com"); 
 		} catch (IOException e) {
 			logger.info("跳转到网易门户失败");
 			e.printStackTrace();
@@ -38,7 +38,7 @@ public class Hao123Controller {
 	
 	@RequestMapping(value = "/wangyi", method = RequestMethod.GET)
 	public void wangyi(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~wangyi~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~wangyi~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("http://www.163.com"); 
 		} catch (IOException e) {
@@ -49,7 +49,7 @@ public class Hao123Controller {
 	
 	@RequestMapping(value = "/qq", method = RequestMethod.GET)
 	public void tencentIndex(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~wangyi~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~wangyi~~~~~~~~~~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("http://www.qq.com"); 
 		} catch (IOException e) {
@@ -59,7 +59,7 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/douyu", method = RequestMethod.GET)
 	public void douyu(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~douyu~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~douyu~~~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("http://www.douyu.com"); 
 		} catch (IOException e) {
@@ -69,7 +69,7 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/iqiyi", method = RequestMethod.GET)
 	public void iqiyi(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~iqiyi~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~iqiyi~~~~~~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("http://www.iqiyi.com"); 
 		} catch (IOException e) {
@@ -79,7 +79,7 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/youku", method = RequestMethod.GET)
 	public void youku(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~youku~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~youku~~~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("http://www.youku.com"); 
 		} catch (IOException e) {
@@ -89,7 +89,7 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/imooc", method = RequestMethod.GET)
 	public void imooc(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~imooc~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~imooc~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("https://www.imooc.com/"); 
 		} catch (IOException e) {
@@ -99,9 +99,9 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/panda", method = RequestMethod.GET)
 	public void panda(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~panda~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info("---------------~~~~~~~~~~~~panda~~~~~~~~"+getIpAddress(req));
 		try {
-			resp.sendRedirect("https://www.panda.tv"); 
+			resp.sendRedirect(new Date()+"https://www.panda.tv"); 
 		} catch (IOException e) {
 			logger.info("跳转到youku门户失败");
 			e.printStackTrace();
@@ -109,7 +109,7 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/yizhibo", method = RequestMethod.GET)
 	public void yizhibo(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~yizhibo~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~yizhibo~~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("http://new.yizhibo.com/"); 
 		} catch (IOException e) {
@@ -119,7 +119,7 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/38hao", method = RequestMethod.GET)
 	public void hao38(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~38hao~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~38hao~~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("http://www.hellozz.cn/"); 
 		} catch (IOException e) {
@@ -129,7 +129,7 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/taobao", method = RequestMethod.GET)
 	public void taobao(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~taobao~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~taobao~~~~~~~~"+getIpAddress(req));
 		try {
 			resp.sendRedirect("https://www.taobao.com/"); 
 		} catch (IOException e) {
@@ -139,12 +139,70 @@ public class Hao123Controller {
 	}
 	@RequestMapping(value = "/jd", method = RequestMethod.GET)
 	public void jd(HttpServletRequest req,HttpServletResponse resp){
-		logger.info("---------------~~~~~~~~~~~~jd~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		logger.info(new Date()+"---------------~~~~~~~~~~~~jd~~~~"+getIpAddress(req));
 		try {
-			resp.sendRedirect("https://www.taobao.com/"); 
+			resp.sendRedirect("https://www.jd.com/"); 
 		} catch (IOException e) {
 			logger.info("跳转到youku门户失败");
 			e.printStackTrace();
 		} 
 	}
+	//  http://edu.51cto.com/?wwwdh0
+	@RequestMapping(value = "/cto51", method = RequestMethod.GET)
+	public void cto51(HttpServletRequest req,HttpServletResponse resp){
+		logger.info(new Date()+"---------------~~~~~~~~~~~~cto51    http://edu.51cto.com/?wwwdh0   ~~~~~~"+getIpAddress(req));
+		try {
+			resp.sendRedirect("http://edu.51cto.com/?wwwdh0"); 
+		} catch (IOException e) {
+			logger.info("跳转到51cto门户失败");
+			e.printStackTrace();
+		} 
+	}	
+	
+//  http://edu.51cto.com/?wwwdh0
+	@RequestMapping(value = "/bjsxt", method = RequestMethod.GET)
+	public void bjsxt(HttpServletRequest req,HttpServletResponse resp){
+		logger.info(new Date()+"---------------~~~~~~~~~~~~cto51    http://edu.51cto.com/?wwwdh0  ~~~~"+getIpAddress(req));
+		try {
+			resp.sendRedirect("http://www.bjsxt.com/"); 
+		} catch (IOException e) {
+			logger.info("跳转到北京尚学堂门户失败");
+			e.printStackTrace();
+		} 
+	}
+	
+    /** 
+     * 获取用户真实IP地址，不使用request.getRemoteAddr();的原因是有可能用户使用了代理软件方式避免真实IP地址, 
+*  
+     * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？ 
+     * 答案是取X-Forwarded-For中第一个非unknown的有效IP字符串。 
+     *  
+     * 如：X-Forwarded-For：192.168.1.110, 192.168.1.120, 192.168.1.130, 
+     * 192.168.1.100 
+     *  
+     * 用户真实IP为： 192.168.1.110 
+     *  
+     * @param request 
+     * @return 
+     */  
+    public static String getIpAddress(HttpServletRequest request) {  
+        String ip = request.getHeader("x-forwarded-for");  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("WL-Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_CLIENT_IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getRemoteAddr();  
+        }  
+        return ip;  
+    }  
+	
 }
