556f3a05acf351f07af567dfcd3235aad94da059
huangleisir <huangleisir@qq.com>
2018-11-04 17:47:28 +0800

    加一些有用的问题

--- src/main/java/com/hl/Hao123Application.java
+++ src/main/java/com/hl/Hao123Application.java
@@ -127,7 +127,7 @@ public class Hao123Application {
 						+ "读未提交（read-uncommitted） 	是 	是 	是\n" + "不可重复读（read-committed） 	否 	是 	是\n"
 						+ "可重复读（repeatable-read） 	否 	否 	是\n" + "串行化（serializable） 	否 	否 	否",
 				"12mysql优化：sql优化；索引优化，执行计划，配置参数优化", "13分布式锁", "15redis,复制,分布式锁,哈希环", "16讲讲现公司的架构", "17数据结构和算法  冒择入希快归堆",
-				"18dubbo", "19zookepper", "20分布式事务", "21敏捷开发", "22linux命令，根据端口找进程", "23zookeeper选举原理",
+				"18dubbo", "19zookepper", "20分布式事务", "21敏捷开发  快速迭代 持续集成", "22linux命令，根据端口找进程", "23zookeeper选举原理",
 				"24consul跟zk有什么区别", "25最新版JDK HashMap的结构", "26springmvc的原理", "27oracle sql实现递归", "28mysql优化",
 				"29执行计划看什么,是否有全表扫描,索引是否生效",
 				"30jvm的数据模型,年代划分,gc算法 \n   https://www.cnblogs.com/kingszelda/p/7226080.html", "32set是怎么实现不重复的",
@@ -145,7 +145,78 @@ public class Hao123Application {
 						+ "3）通过反射机制获得动态代理类的构造函数，其唯一参数类型是调用处理器接口类型\n" + "4）通过构造函数创建动态代理类实例，构造时调用处理器对象作为参数被传入",
 				"41适配器模式如何实现", "42桥接模式如何实现", "43策略模式如何实现",
 				"44门面模式如何实现 \n  https://blog.csdn.net/xingjiarong/article/details/50066133", "45命令行模式如何实现",
-				"46过滤器中如何重新整理请求头，请求体中的数据 用Servlet" + "RequestWrapper,因为用了new来创建Wrapper实例，所以不会影响原始的request，和response");
+				"46过滤器中如何重新整理请求头，请求体中的数据 用Servlet" + "RequestWrapper,因为用了new来创建Wrapper实例，所以不会影响原始的request，和response",
+				"47数据库优化 https://ke.qq.com/course/255238 这个里面很简洁的讲了主从配置读写分离\n"
+						+ "I  经常查询并且区分力度大的字段加索引 II 索引要有效，不能索引失效 III 去掉无必要关联 IV  去掉select *\n"
+						+ "VI 必须使用InnoDB引擎   VII 禁止使用视图，触发器，存储过程 避免DB压力过大 IX 禁止使用外键约束，用程序控制关联性，增删改十分影响SQL性能，大并发场景下性能优先。 X 禁止使用小数存储货币金额，避免金额对不上 XI 尽量使用小类型字段 boolean char tinyint int bigint long XII sql优化最基本的原则，就是让数据库尽早、高效率的过滤数据，避免无效的运算，具体的手段比较多，需要根据不同的数据库来确定实现方案。 XIII 对大数据量的表尽量避免join，也就是不用表本身的join功能，而是自己单独查一张表，再去循环查询另一张表。（上亿条数据）\n"
+						+ "6.1 分库分表，读写分离 ，主从复制怎么做\n" + "7.数据库索引，sql优化 布尔学院那里的总结比较好",
+				"48分布式：dubbo架构和zk原理\n" + "\n"
+						+ "dubbo 源码 ----------------------duboo底层核心知识点------------------------------ tcp的单工、半双工、全双工、编码、解码、粘包、拆包。网络通信netty nio 阻塞、非阻塞、异步、同步、线程池、boss线程、work线程。集群负载均衡directory、router、cluster、loadbalance。zookeeper与zkClient、持久节点 和 临时节点。服务的本地暴露和远程暴露、服务降级、服务灰度发布。 dubbo雪崩 https://www.jianshu.com/p/48d3b1926086   dubbo雪崩很有可能是服务端返回超时，导致重连，retries默认是3，导致后端连接时最高达到正常的3倍，很有可能超过最大连接数，另外导致不幂等，一是后端 优化，缩短响应时间，还有增加超时时间，另外将重试次数设置为0.\n"
+						+ "\n" + "dubboX 以及各种PRC框架横向比较http://blog.csdn.net/i_will_try/article/details/76212234",
+				"49事务  这个曾经是我最头疼的 http://blog.csdn.net/liaohaojian/article/details/68488150?locationNum=8&fps=1 事务有四大点 1.事务原则  ACID 原子性 一致性 隔离性 持久性 2. 事务类型  I 数据库事务分为本地事务和全局事务  II java事务  jdbc事务和JTA事务   III 声明式事务和编程式事务 3.spring五大事务隔离级别\n"
+						+ "\n"
+						+ ".Spring事务隔离级别：spring有五大隔离级别，其在TransactionDefinition接口中定义。看源码可知，其默isolation_default（底层数据库默认级别），其他四个隔离级别跟数据库隔离级别一致。\n"
+						+ "ISOLATION_DEFAULT：用底层数据库的默认隔离级别，数据库管理员设置什么就是什么\n"
+						+ "ISOLATION_READ_UNCOMMITTED（未提交读）：最低隔离级别、事务未提交前，就可被其他事务读取（会出现幻读、脏读、不可重复读）\n"
+						+ "ISOLATION_READ_COMMITTED（提交读）：一个事务提交后才能被其他事务读取到（该隔离级别禁止其他事务读取到未提交事务的数据、所以还是会造成幻读、不可重复读）、sql server默认级别\n"
+						+ "ISOLATION_REPEATABLE_READ（可重复读）：可重复读，保证多次读取同一个数据时，其值都和事务开始时候的内容是一致，禁止读取到别的事务未提交的数据（该隔离基本可防止脏读，不可重复读（重点在修改），但会出现幻读（重点在增加与删除））（MySql默认级别，更改可通过set transaction isolation level 级别）\n"
+						+ "ISOLATION_SERIALIZABLE（序列化）：代价最高最可靠的隔离级别（该隔离级别能防止脏读、不可重复读、幻读）\n"
+						+ "丢失更新：两个事务同时更新一行数据，最后一个事务的更新会覆盖掉第一个事务的更新，从而导致第一个事务更新的数据丢失，这是由于没有加锁造成的；\n"
+						+ "幻读：同样的事务操作过程中，不同时间段多次（不同事务）读取同一数据，读取到的内容不一致（一般是行数变多或变少）。\n"
+						+ "脏读：一个事务读取到另外一个未提及事务的内容，即为脏读。\n" + "不可重复读：同一事务中，多次读取内容不一致（一般行数不变，而内容变了）。\n"
+						+ "幻读与不可重复读的区别：幻读的重点在于插入与删除，即第二次查询会发现比第一次查询数据变少或者变多了，以至于给人一种幻象一样，而不可重复读重点在于修改，即第二次查询会发现查询结果比第一次查询结果不一致，即第一次结果已经不可重现了。\n"
+						+ "数据库隔离级别越高，执行代价越高，并发执行能力越差，因此在实际项目开发使用时要综合考虑，为了考虑并发性能一般使用提交读隔离级别，它能避免丢失更新和脏读，尽管不可重复读和幻读不能避免，但可以在可能出现的场合使用悲观锁或乐观锁来解决这些问题。\n"
+						+ "悲观锁与乐观锁可参考：http://blog.csdn.net/liaohaojian/article/details/62416972\n" + "",
+				"50分布式\n" + "\n"
+						+ "10.1 分布式锁 分布式锁:利用db行锁 ， redis锁   https://www.cnblogs.com/linjiqin/p/8003838.html  把这里面的工程要背下来 关键点：1 用setnx加锁，key要唯一，要加有效时间，且不能太长，避免死锁；要加随机数，解锁的时候，根据这个随机数判断是否是自己加的锁； 2. 释放锁，要用lua脚本，根据key取value，比较value，然后删除key即释放锁，一定要保证原子型，所以用lua脚本\n"
+						+ "和zookeeper锁\n" + "\n"
+						+ "保证锁节点（lock root node）这个父根节点的存在，这个节点是每个要获取lock客户端共用的，这个节点是PERSISTENT的。 第一次需要创建本客户端要获取lock的节点，调用 create( )，并设置 节点为EPHEMERAL_SEQUENTIAL类型，表示该节点为临时的和顺序的。如果获取锁的节点挂掉，则该节点自动失效，可以让其他节点获取锁。\n"
+						+ "\n" + "在父锁节点（lock root node）上调用 getChildren( ) ，不需要设置监视标志。 (为了避免“羊群效应”).\n" + "\n"
+						+ "按照Fair竞争的原则，将步骤3中的子节点（要获取锁的节点）按照节点顺序的大小做排序，取出编号最小的一个节点做为lock的owner，判断自己的节点id 是否就为owner id，如果是则返回，lock成功。如果不是则调用 exists( )监听比自己小的前一位的id，关注它锁释放的操作（也就是exist watch）。",
+				"51jvm运行时数据图 GC 内存溢出 CPU高等解决办法 图 理解这个图 对理解GC 以及多线程非常有好处。 特别是锁，锁的是对象还是类 http://blog.csdn.net/loveslmy/article/details/46820929  这篇博客总结的非常好，非常喜欢前面那张图，图旁边有箭头指向，并且加了注释，非常好。 另外非常喜欢他的GC描述，讲的调理非常清晰，年轻代使用标记清除算法，因为年轻代大多数对象都是要被清理掉的，只有少量会存活下来，所以用mark-sweep GC 算法，年老代则是只有少部分对象需要被清理，所以使用标记整理算法（不会产生内存碎片）。 还有他的收集器的总结真的是绝了，GC收集器，从来没有这么清晰的明白过收集器。特别是那张图，上次在那个TCL国际E城那里面试，那个视力不好的家伙估计也 不懂这块的知识，很不耐烦的直接问，IO密集型和运算密集型分别使用什么类型的收集器，这东西是汪文敏那种家伙怎么都不会懂的，IO密集场景下用并发， 高并发嘛，运算密集型用并行，oracle里面不是用parallel来并行执行sql，加快执行时间吗。 另外就是收集器是如何在java命令中去配置的。 http://blog.csdn.net/heyutao007/article/details/38151581  这一篇也是讲的不错的 -XX：survivalRatio=8（默认值） 画个图 eden与From与To区域比例8：1：1  如果设置10 证个young generation 1200M 那分别是1000M 100M 100M GC  -XX:NewRatio 默认为4  老年代与新生代的比值为4:1  就是说老年代占整个堆内存的80%，剩下的新生代占20%，这个比值是个反的，不要被New所 误导 VM -XX:NewRatio=4（老年代与新生代的比值）、-XX:SurvivorRatio=8（eden与一个survivor的比值）参数含义 https://www.cnblogs.com/redcreen/archive/2011/05/04/2037057.html 这个里面jvm参数来历讲的比较好\n"
+						+ "\n" + "查看dump文件，，用IBM Analyzer打开.hprof文件\n" + "\n"
+						+ " -XX:+HeapDumpOnOutOfMemoryError 此参数是帮助生成dump文件，程序启动后直到抛出OOM异常，异常抛出后，在程序的classpath下会生成一个以.hprof结尾的文件，如 java_pid4504.hprof文件， 这就是我们需要的dump文件。这个文件在实际生产环境中比较大，，打开这个文件就可以分析java内存溢出点。 获取到这个.hprof文件之后，使用IBM的heapAnalyzer，这个IBM开发的强大的内存dump分析工具，分析我们的.hprof文件来打开分析找到可能的内存泄漏点。\n"
+						+ "\n" + "打开这个.hprof文件，就可以看到这个高亮显示的部分就是可能的内存泄漏点， 打开 tree viwer 就能看到一个占用内存排序的列表。\n" + "\n"
+						+ "内存溢出 两种可能 有大对象，堆内存空间不够，2.死循环，往某个容器里面拼命加东西,死锁，   3.-xmx参数设置过小\n" + "\n"
+						+ "CPU高的解决办法  https://www.cnblogs.com/guoyuqiangf8/p/3545687.html 终于知道那个鼓泡那个家伙一口气念了那么多都是什么了，jinfo,jps,jstack,jstat,jmap,jconsole j后面的ipss mc  还有 JProfiler、Optimizeit Profiler 查看内存泄漏的 http://blog.csdn.net/caihaijiang/article/details/6084325",
+				"52oppo问到的几个新问题： 1.线程死锁怎么解决，回答的时候有点忘了，说是避免多个线程多共享资源的争用。 2.set是怎么实现避免重复的； 3.error和exception的区别， 4.9个线程读一个线程写的安全分析。 redis锁的缺陷\n"
+						+ "HashSet：HashSet的实现原理 https://www.cnblogs.com/xwdreamer/archive/2012/06/03/2532999.html  昨天在oppo卓越后海中心面试，我当时就 傻了，set是怎么保证数据不重复的，今天看了源码恍然大悟，源码set add方法添加的元素，是作为key来存放在map里面的，这样就保证了元素的唯一性， 另外因为map支持key为null，所以set也支持添加null元素。\n"
+						+ "死锁产生的原因及如何避免 http://blog.csdn.net/ls5718/article/details/51896159 死锁及解决方案 http://blog.csdn.net/liujianfei526/article/details/51933162   鸵鸟方案挺有一丝，鸵鸟方案就是啥都不干\n"
+						+ "https://zhidao.baidu.com/question/1893799901594511140.html  鸵鸟心态\n"
+						+ "oppo复试问到的一些问题：1.客户端自动更新是怎么做的。 2.大并发场景持续优化怎么做，分库分表忘了讲，redis进一步优化，增加redis节点，用多个key解决 key过热的问题。增加tomcat配置的线程数。架构图。 3.幂等 4.分布式锁   5.多线程处理请求（接口分离，每个接口用一个独立的线程来处理）\n"
+						+ "2018-02-02 16:53 几天上午通知oppo复试通过，还是那句话，级别不到位，钱不给到位，入职没意义。 所以自己的水平也要不断提高。\n"
+						+ "2018-02-01 8:55 昨天oppo复试 问了service层中用到的设计模式，这个有点意思。 最王道的问题还是终极优化的问题，问得我绝望了。发现张开涛的亿级网站架构核心技术，这本书要拿出来好好的提炼提炼，这些大型互联网企业最终的技术都是 统一的。高可用高并发系统 提炼总结一下开涛的京东经验，虽然没有阿里牛逼，但是还是可以的。 这好比是登顶珠穆朗玛，你要找一个盘山公路上去，如果没有路，你要先自己给自己铺路。这条盘山公路最终是给自己走的，而且是又双叒叕来回走的，所以务必修得好。修的像走电影院的地毯上一样。 扩容，一是集群节点可以扩，二是DB分库分表可以扩容，三是redis可以扩容，为了key过热，可以用多个key。单独节点里面tomcat的线程数可以进一步增加。",
+				"53JVM运行时数据区\n"
+						+ "--   纯文字描述，等同于面试时候的演讲 讲述JVM运行时数据区不同于 讲述JVM内存模型   这是两个不同的概念 ----http://blog.csdn.net/tjiyu/article/details/53915869---\n"
+						+ "--------正式开始，清一清嗓子 首先说，jvm运行时的数据区分为两块，一个是所有线程公用的区域：1.堆内存，存放所有Java 对象，用jvm垃圾回收机制来管理。 2.方法区，存储包括jvm加载的每一个 类的信息，包括常量池，成员变量和方法。 另一个是每一个栈所私有的区域：   本地方法栈，虚拟机栈和程序计数器。这些是每个线程私有的，不会与其他线程共有。在新建线程时创建，在线程结束时销毁。\n"
+						+ "先说说这个程序计数器 我们的jvm内部在执行多线程任务的时候，他用的是CPU时间片的轮流分配来决定哪一个线程作为当前线程来执行程序代码。 这个计数器就是骑存储该线程需要执行的字节码指令的地址，到了该线程来执行的时候，JVM字节码解释器会来取该程序计数器的字节码指令的地址来寻找要执行的程序。\n"
+						+ "再说Java虚拟机栈\n"
+						+ "他是为方法调用而服务的，存放的是线程的栈帧，当方法调用的时候，栈帧被创建，当方法调用结束的时候，栈帧被销毁， 栈帧是方法运行时的基础结构，它里面存放的是方法执行时候逇一些信息，局部变量表、操作数栈、动态连接、方法出口。\n"
+						+ "HotSpot VM通过\"-Xss\"参数设置JVM栈内存空间大小\n"
+						+ "本地方法栈    想起了昨天看Object类的时候，看到了他下面的一个本地方法hashCode，没有代码实现。 为什么会这样呢，因为这个本地方法指的是 用Java语言之外的语言来编写的方法，所以不需要用Java代码来实现。\n"
+						+ "谈堆内存的话，那就是JVM内存模型的问题了，那是一个更具体的话题。\n" + "-----以上\n",
+				"54请讲讲ThreadLocal下面的四个方法. ThreadLocal用法详解和原理\n" + "\n" + "一、用法\n"
+						+ "ThreadLocal用于保存某个线程共享变量：对于同一个static ThreadLocal，不同线程只能从中get，set，remove自己的变量，而不会影响其他线程的变量。\n"
+						+ "\n" + "1、ThreadLocal.get: 获取ThreadLocal中当前线程共享变量的值。\n"
+						+ "2、ThreadLocal.set: 设置ThreadLocal中当前线程共享变量的值。\n"
+						+ "3、ThreadLocal.remove: 移除ThreadLocal中当前线程共享变量的值。\n"
+						+ "4、ThreadLocal.initialValue: ThreadLocal没有被当前线程赋值时或当前线程刚调用remove方法后调用get方法，返回此方法值。",
+				"55mybatis中的namespace有什么作用你这样来记忆，mapper文件跟dao接口是一一对应的，dao接口完整名对应namespace，dao的方法名对应sql中的id，就行了\n" + "\n"
+						+ "mybatis 中mapper 的namespace有什么用 5 我自己定义的一个sql语句配置 可是我不懂这里的namespace有什么用 SELECT t.* FROM T_article t WHERE t.flag = '1' ORDER BY t.createtime D... 展开 lanmo970 | 浏览 23220 次 推荐于2016-03-01 17:04:57 最佳答案 楼主： 在mybatis中，映射文件中的namespace是用于绑定Dao接口的，即面向接口编程。 当你的namespace绑定接口后，你可以不用写接口实现类，mybatis会通过该绑定自动 帮你找到对应要执行的SQL语句，如下： 假设定义了IArticeDAO接口 public interface IArticleDAO { List selectAllArticle(); } 对于映射文件如下： SELECT t.* FROM T_article t WHERE t.flag = '1' ORDER BY t.createtime DESC 请注意接口中的方法与映射文件中的SQL语句的ID一一对应 。 则在代码中可以直接使用IArticeDAO面向接口编程而不需要再编写实现类。 有问题欢迎提问，满意请采纳，谢谢！",
+				"56linux内核参数优化* 修改最大文件句柄打开数&最大可开进程数调优\n" + "\n" + "修改最大文件句柄打开数&最大可开进程数调优\n" + "    ulimit –u 65535\n"
+						+ "    ulimit –n 65535\n" + "查看： ulimit -a\n" + "\n"
+						+ "lsof -n |awk '{print $2}'|sort|uniq -c |sort -nr|more   这个可以看最大可打开文件句柄数的统计结果。\n"
+						+ "然后用 ps -ef|grep pid_num  查看到底是哪一个进程占用了这么多的打开文件数\n" + "\n" + "\n"
+						+ "聊聊jps这个命令，这个命令可以查看当前活着的java进程，这个就有意思了，springboot进程是可以发现的。 以前都用\n"
+						+ "ps -ef|grep member.jar  这个方式，太low 了，直接用jps -l,真好用。\n"
+						+ "---------------------------------------------------------------------------\n"
+						+ "注：jps命令有个地方很不好，似乎只能显示当前用户的java进程，要显示其他用户的还是只能用unix/linux的ps命令。\n" + "\n" + "\n"
+						+ "TCP传输参数调优\n" + "执行命令(root用户) ：\n" + "         vi /etc/sysctl.conf \n" + "   修改内容：\n"
+						+ "         net.ipv4.tcp_max_tw_buckets = 6000\n"
+						+ "              net.ipv4.ip_local_port_range = 1024 65000\n"
+						+ "              net.ipv4.tcp_tw_recycle = 1\n" + "              net.ipv4.tcp_tw_reuse = 1\n"
+						+ "              net.ipv4.tcp_syncookies = 1\n" + "              net.core.somaxconn = 262144\n"
+						+ "          以上解决，高并发下linux访问流量上不去的问题   \n" + "");
 		int i = RandomUtils.nextInt(list.size());
 		return list.get(i);
 	}
