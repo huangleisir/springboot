eb1b183fc31ca51fa89ea08abc28deac5578a425
huangleisir <huangleisir@qq.com>
2018-11-14 20:34:13 +0800

    打包不成功

--- src/main/java/util/Mail.java
+++ src/main/java/util/Mail.java
@@ -172,16 +172,14 @@ public class Mail {
 		return true;
 	}
 
-	public static void main(String[] args) {
-		String smtp = "smtp.126.com";// smtp服务器
-		String from = "xxxxxxx@126.com";// 邮件显示名称
-		String to = "xxxx@qq.com";// 收件人的邮件地址，必须是真实地址
-		String copyto = "";// 抄送人邮件地址
-		String subject = "测试邮件";// 邮件标题
-		String content = "luo ri de 你好！";// 邮件内容
-		String username = "xx@126.com";// 发件人真实的账户名
-		String password = "xxxx";// 发件人密码
-		Mail.sendAndCc(smtp, from, to, copyto, subject, content, username, password);
-	}
+	/*
+	 * public static void main(String[] args) { String smtp = "smtp.126.com";//
+	 * smtp服务器 String from = "xxxxxxx@126.com";// 邮件显示名称 String to =
+	 * "xxxx@qq.com";// 收件人的邮件地址，必须是真实地址 String copyto = "";// 抄送人邮件地址 String
+	 * subject = "测试邮件";// 邮件标题 String content = "luo ri de 你好！";// 邮件内容 String
+	 * username = "xx@126.com";// 发件人真实的账户名 String password = "xxxx";// 发件人密码
+	 * Mail.sendAndCc(smtp, from, to, copyto, subject, content, username, password);
+	 * }
+	 */
 
 }
