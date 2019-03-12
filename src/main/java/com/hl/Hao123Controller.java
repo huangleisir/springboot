b6de92c3f9bfd3d9edc40820f3b5bd03e8975274
huangleisir <huangleisir@qq.com>
2019-03-12 20:29:12 +0800

    add moto  xiaofeng

--- src/main/java/com/hl/Hao123Controller.java
+++ src/main/java/com/hl/Hao123Controller.java
@@ -24,7 +24,6 @@ import util.SimpleThreadPoolUtils;
 @RestController
 public class Hao123Controller {
     private static final Logger logger = LoggerFactory.getLogger(Hao123Controller.class);
-
     /**
      * http://localhost:8080/&#x4e2d;&#x6587; &#x4e0d;&#x9519;&#xff0c;&#x662f;&#x53ef;&#x4ee5;&#x652f;&#x6301;&#x4e2d;&#x6587;&#x7684;
      *
