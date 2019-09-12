3cbd8ef55a430f31c6f4e6b5e5fe3f3626c845a4
DELL <DELL@DESKTOP-L6MR0C2>
2019-09-12 17:57:40 +0800

    syso 改成  log.info

--- src/test/java/ThreadPoolTest.java
+++ src/test/java/ThreadPoolTest.java
@@ -3,12 +3,16 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class ThreadPoolTest implements Runnable {
+	static Logger log = LoggerFactory.getLogger(ThreadPoolTest.class);
 	@Override
     public void run() {
 		 synchronized(this) {
 			 try{
-			 System.out.println(Thread.currentThread().getName());
+				 log.info(Thread.currentThread().getName());
 			 Thread.sleep(3000);
 			 }catch (InterruptedException e){
 			 e.printStackTrace();
@@ -22,21 +26,21 @@ public class ThreadPoolTest implements Runnable {
     	executor.setRejectedExecutionHandler(new RejectedExecutionHandler() {
             @Override
             public void rejectedExecution(Runnable r, ThreadPoolExecutor executor2) {
-            	System.out.println("文件迁移线程池阻塞队列已满，开始等待进入线程池");
+            	log.info("文件迁移线程池阻塞队列已满，开始等待进入线程池");
                 if (!executor2.isShutdown()) {
                     try {
                         executor2.getQueue().put(r);
                     } catch (InterruptedException e) {
-                    	System.out.println("尝试加入线程池等待错误");
+                    	log.info("尝试加入线程池等待错误");
                     }
                 }
             }
         });
     	for (int i = 0; i <= 20; i++) {
-    		System.out.println("執行第"+i+"個");
+    		log.info("執行第"+i+"個");
     		executor.execute(new Thread(new ThreadPoolTest(),"TestThread".concat(""+i)));
     		int threadSize = queue.size();
-    		System.out.println("线程队列大小为-->"+threadSize);
+    		log.info("线程队列大小为-->"+threadSize);
     	}
     	executor.shutdown();
     }
