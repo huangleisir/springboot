dc22ce647af72fbb52469948e3b30696cae2a92b
huangleisir@qq.com <huangleisir@qq.com>
2019-09-17 15:56:07 +0800

    update log

--- src/main/java/com/hl/Hao123Controller.java
+++ src/main/java/com/hl/Hao123Controller.java
@@ -25,7 +25,7 @@ import util.SimpleThreadPoolUtils;
 public class Hao123Controller {
     private static final Logger logger = LoggerFactory.getLogger(Hao123Controller.class);
     /**
-     * http://localhost:8080/&#x4e2d;&#x6587; &#x4e0d;&#x9519;&#xff0c;&#x662f;&#x53ef;&#x4ee5;&#x652f;&#x6301;&#x4e2d;&#x6587;&#x7684;
+     * http://localhost:9080/
      *
      * @return
      * @throws IOException
