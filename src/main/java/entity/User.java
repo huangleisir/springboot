9a897598a31159c61e87a693df3cce6033713879
huangleisir@qq.com <huangleisir@qq.com>
2019-09-17 15:36:46 +0800

    // Lombok更类似于一种IDE插件，项目也需要依赖相应的jar包。Lombok依赖jar包是因为编译时要用它的注解， // @Data太过残暴（因为@Data集合了@ToString、@EqualsAndHashCode、@Getter/@Setter、@RequiredArgsConstructor的所有特性） //@NoArgsConstructor, @RequiredArgsConstructor and @AllArgsConstructor //无参构造器、部分参数构造器、全参构造器。Lombok没法实现多种参数构造器的重载。 //@Cleanup   该注解能帮助我们自动调用close()方法，很大的简化了代码。 // @NonNull //该注解用在属性或构造器上，Lombok会生成一个非空的声明，可用于校验参数，能帮助避免空指针。

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,6 +1,20 @@
 package entity;
 import lombok.Data;
-@Data
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+// Lombok更类似于一种IDE插件，项目也需要依赖相应的jar包。Lombok依赖jar包是因为编译时要用它的注解，
+// @Data太过残暴（因为@Data集合了@ToString、@EqualsAndHashCode、@Getter/@Setter、@RequiredArgsConstructor的所有特性）
+//@NoArgsConstructor, @RequiredArgsConstructor and @AllArgsConstructor
+//无参构造器、部分参数构造器、全参构造器。Lombok没法实现多种参数构造器的重载。
+//@Cleanup   该注解能帮助我们自动调用close()方法，很大的简化了代码。
+// @NonNull
+//该注解用在属性或构造器上，Lombok会生成一个非空的声明，可用于校验参数，能帮助避免空指针。
+@Getter
+@Setter
+@ToString
+//@Data
 public class User{
     private String name;
     private int age;
