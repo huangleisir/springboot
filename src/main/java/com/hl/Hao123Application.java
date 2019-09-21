1f2270e680465b5c94c9b039c1838e43bd97794f
huangleisir <huangleisir@qq.com>
2019-09-21 09:52:42 +0800

    update log

--- src/main/java/com/hl/Hao123Application.java
+++ src/main/java/com/hl/Hao123Application.java
@@ -1329,7 +1329,24 @@ public class Hao123Application {
                 "454 TCP的三次握手和四次挥手，以及为什么要三次握手，而不是二次？",
                 "455 redis的单线程特性有什么优缺点？",
                 "456 对Paxos算法了解多少？",
-                "457",
+                "457 serialVersionUID作用：\n" +
+                        "序列化时为了保持版本的兼容性，即在版本升级时反序列化仍保持对象的唯一性。\n" +
+                        "有两种生成方式：\n" +
+                        "一个是默认的1L，比如：private static final long serialVersionUID = 1L;\n" +
+                        "一个是根据类名、接口名、成员方法及属性等来生成一个64位的哈希字段，比如：\n" +
+                        "private static final long serialVersionUID = xxxxL;\n" +
+                        "当你一个类实现了Serializable接口，如果没有定义serialVersionUID，Eclipse会提供这个\n" +
+                        "提示功能告诉你去定义 。在Eclipse中点击类中warning的图标一下，Eclipse就会\n" +
+                        "自动给定两种生成的方式。如果不想定义它，在Eclipse的设置中也\n" +
+                        "可以把它关掉的，设置如下：\n" +
+                        "Window ==> Preferences ==> Java ==> Compiler ==> Error/Warnings ==>\n" +
+                        "Potential programming problems\n" +
+                        "将Serializable class without serialVersionUID的warning改成ignore即可。\n" +
+                        "如果你没有考虑到兼容性问题时，就把它关掉，不过有这个功能是好的，只要任何类别实现了Serializable这个接口的话，如果没有加入serialVersionUID，\n" +
+                        "Eclipse都会给你warning提示，这个serialVersionUID为了让该类别Serializable向后兼容。\n" +
+                        "如果你的类Serialized存到硬盘上面后，可是后来你却更改了类别的field(增加或减少或改名)，当你Deserialize时，就会出现Exception的，\n" +
+                        "这样就会造成不兼容性的问题。\n" +
+                        "但当serialVersionUID相同时，它就会将不一样的field以type的预设值Deserialize，可避开不兼容性问题。",
                 "458",
                 "459",
                 "460",
